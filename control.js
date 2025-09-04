76029a0f4f7afc066576dc1b84bcdc87dce85707
Iland <iland12maimom@gmail.com>
2025-09-04 11:11:11 +0530

    first commit

---
+++ control.js
@@ -0,0 +1,34 @@
+const logIn = document.querySelector(".login");
+const register = document.querySelector(".register");
+const loginHere = document.querySelector(".term2 a");
+
+loginHere.addEventListener("click", () => {
+  logIn.style.display = "block";
+  register.style.display = "none";
+});
+
+const n_btn = document.querySelector(".navbar button");
+
+n_btn.addEventListener("click", (e) => {
+  e.preventDefault();
+  logIn.style.display = "block";
+  register.style.display = "none";
+});
+
+const l_register = document.querySelector(".content #r");
+l_register.addEventListener("click", (e) => {
+  e.preventDefault();
+  register.style.display = "block";
+  logIn.style.display = "none";
+});
+
+const password = document.querySelector("#password");
+const confirmPassword = document.querySelector("#confirmPassword");
+
+if (password.value.length === 8) {
+  password.setAttribute("title", "✅ Password confirmed!");
+  password.style.borderColor = "green";
+} else {
+  password.setAttribute.title = "❌ Must be exactly 8 characters";
+  password.style.borderColor = "red";
+}
